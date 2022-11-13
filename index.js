b1184e937dc1ea9678602583bfbc5a4a17e4eb01
LeoZhangYYDS <zhanglingxiao1991@gmail.com>
2022-11-14 10:42:03 +1100

    most code finished

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require("express");
-const ejs = require("ejs");
 const app = express();
+const ejs = require("ejs");
+const fetch = require("node-fetch");
 
 // middleware
 app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index.ejs");
+});
+
+//  k to cel
+function kToC(k) {
+  return (k - 273.15).toFixed(2);
+}
+
+//api key
+let myKey = "c6838cc306630664ea3e7e947a7ea9bc";
+
+app.get("/response", async (req, res) => {
+  let { city } = req.query;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
+  let response = await fetch(url);
+  let data = await response.json;
+  let { temp } = data.main;
+  let cTemp = ktoc(temp);
+  console.log(data);
+  res.render("weather.ejs", { data, cTemp });
 });
 
 app.listen(3000, () => {
