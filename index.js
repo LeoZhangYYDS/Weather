821c3f08bb7c373e457896b07537bda2287d2585
LeoZhangYYDS <zhanglingxiao1991@gmail.com>
2022-11-14 14:51:00 +1100

    weather api done

--- index.js
+++ index.js
@@ -4,30 +4,34 @@ const ejs = require("ejs");
 const fetch = require("node-fetch");
 
 // middleware
-app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+//homepage
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-//  k to cel
+// kelvin to celsius
 function kToC(k) {
   return (k - 273.15).toFixed(2);
 }
 
 //api key
-let myKey = "c6838cc306630664ea3e7e947a7ea9bc";
+const myKey = "c6838cc306630664ea3e7e947a7ea9bc";
 
+//pull data from weather api and show data on weather.ejs
 app.get("/response", async (req, res) => {
   let { city } = req.query;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
-  let response = await fetch(url);
-  let data = await response.json;
-  let { temp } = data.main;
-  let cTemp = ktoc(temp);
-  console.log(data);
-  res.render("weather.ejs", { data, cTemp });
+  let d = await fetch(url);
+  let djs = await d.json();
+
+  //use function to convert tempture to celsius
+  let kTemp = djs.main.temp;
+  let cTemp = kToC(kTemp);
+  console.log(djs);
+  res.render("weather.ejs", { djs, cTemp });
 });
 
 app.listen(3000, () => {
